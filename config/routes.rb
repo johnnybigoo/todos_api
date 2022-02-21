045663635a0cd44a0bae528a24a32f6ee3b8c092
johnnybigoo <jotalico@icloud.com>
2022-02-20 23:09:01 -0500

    DigitalOcean tutorial #02 => Authentication Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :todos do
     resources :items
+		post 'auth/login', to: 'authentication#authenticate'
+		post 'signup', to: 'users#create'
   end
 end
 
