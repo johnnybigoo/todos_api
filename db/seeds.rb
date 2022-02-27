38e20c6da306159d08545485b0c17e3c2424b6e5
johnnybigoo <jotalico@icloud.com>
2022-02-27 15:17:48 -0500

    DigitalOcean tutorial #04 => Fixing db:seeds issue.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+user = User.create(name: 'User test', email: 'user@mail.com', password: '123456', password_confirmation: '123456')
+
 # seed 50 records
 50.times do
-  todo = Todo.create(title: Faker::Lorem.word, created_by: User.first.id)
+  todo = Todo.create(title: Faker::Lorem.word, created_by: user.id)
   todo.items.create(name: Faker::Lorem.word, done: false)
 end
